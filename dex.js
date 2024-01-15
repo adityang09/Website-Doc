e9712ebc4c0b03b20f92abb7994be0cbe6f256e4
y-jatin <126376762+y-jatin@users.noreply.github.com>
2024-01-15 17:09:34 +0530

    Update dex.js

--- dex.js
+++ dex.js
@@ -1,6 +1,4 @@
-// Add this to your index.js file
 $(document).ready(function () {
-  // Form validation
   $('#contactForm').submit(function (event) {
     const name = $('#name').val();
     const email = $('#email').val();
